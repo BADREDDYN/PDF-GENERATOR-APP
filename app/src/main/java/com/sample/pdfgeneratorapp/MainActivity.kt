bd3c1f1cf523d68663cae2a98b8ccb1683c6872a
BADREDDYN <badreddynelhirch@gmail.com>
2022-10-15 21:31:04 +0100

    Finish.

--- app/src/main/java/com/sample/pdfgeneratorapp/MainActivity.kt
+++ app/src/main/java/com/sample/pdfgeneratorapp/MainActivity.kt
@@ -1,24 +1,98 @@
 package com.sample.pdfgeneratorapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.itextpdf.text.Document
+import com.itextpdf.text.Paragraph
+import com.itextpdf.text.pdf.PdfWriter
 import com.sample.pdfgeneratorapp.databinding.ActivityMainBinding
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     //Binding
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+
+    private val storageCode = 1001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         //Binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //Get permission and generate the PDF
+        binding.btnGeneratePdf.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_DENIED
+                ) {
+                    val permission = arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    requestPermissions(permission, storageCode)
+                } else {
+                    savePDF()
+                }
+            } else {
+                savePDF()
+            }
+        }
 
 
+    }
 
+    private fun savePDF() {
+        val mDoc = Document()
+        val mFileName = SimpleDateFormat("yyyMMdd_HHmmss", Locale.getDefault())
+            .format(System.currentTimeMillis())
+
+        val mFilePath = Environment.getExternalStorageDirectory().toString() + "/" + mFileName + ".pdf"
+
+        try {
+
+            PdfWriter.getInstance(mDoc, FileOutputStream(mFilePath))
+            mDoc.open()
+
+            val data = binding.edtPdfData.text.toString().trim()
+            mDoc.addAuthor("${R.string.app_name}")
+            mDoc.add(Paragraph(data))
+            mDoc.close()
+            "$mFileName.pdf\n is create to \n$mFilePath".shortToast()
+
+        } catch (ex: Exception) {
+            ex.message.toString().shortSnackBar()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            storageCode -> {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    savePDF()
+                } else {
+                    "Permission denied!".shortSnackBar()
+                }
+            }
+        }
+    }
+
+    private fun String.shortSnackBar() {
+        Snackbar.make(binding.root, this, Snackbar.LENGTH_SHORT).show()
+    }
+    private fun String.shortToast() {
+        Toast.makeText(this@MainActivity, this, Toast.LENGTH_SHORT).show()
     }
 }
