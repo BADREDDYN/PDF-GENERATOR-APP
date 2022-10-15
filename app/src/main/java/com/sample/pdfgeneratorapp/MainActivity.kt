abd02a9b9f27223da1df72a7c29bea9349a156b0
BADREDDYN <badreddynelhirch@gmail.com>
2022-10-15 20:30:30 +0100

    View Binding.

--- app/src/main/java/com/sample/pdfgeneratorapp/MainActivity.kt
+++ app/src/main/java/com/sample/pdfgeneratorapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.sample.pdfgeneratorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sample.pdfgeneratorapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //Binding
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
     }
 }
